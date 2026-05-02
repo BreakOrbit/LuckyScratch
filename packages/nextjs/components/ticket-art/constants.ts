fe00e330d3b221addff18b15c662ec879efc4a3a
yangyang <1348379987@qq.com>
2026-05-02 05:11:06 +0000

    fix: update TICKET_ART_FRAME_CLASS aspect ratio and fallback URL

--- packages/nextjs/components/ticket-art/constants.ts
+++ packages/nextjs/components/ticket-art/constants.ts
@@ -1,4 +1,4 @@
-export const TICKET_ART_FRAME_CLASS = "relative aspect-[9/16] overflow-hidden";
+export const TICKET_ART_FRAME_CLASS = "relative aspect-[3/4] overflow-hidden";
 export const TICKET_ART_IMAGE_CLASS = "h-full w-full object-cover";
 export const TICKET_ART_FALLBACK_URL =
-  "https://lh3.googleusercontent.com/aida-public/AB6AXuCa81_llmwYhIHhOrGNhGlQmDeaH7Wiiz1lze6v73dEM3AjVEmNp3t7zTO6W1OHuosSPgaS-XPJDBj0Hi7Jy6T4hDmD7_-NnetVoWBGxyEEF6axbmQ5w_-YVbyLuKTLkQYhnyOniysfPtiFv_S70dnG8DxiPJHo5WwpM8vnkCUIkKqFz5QhDAW22MYPb0x6Vb7vXhTYxS9h56Og0sgl6zKEUKKUzdhUVUx8u6I838-qS4i5DRZMP0X2cArL-xpC5LhADStOmnfHBNEN";
+  "https://lh3.googleusercontent.com/aida-public/AB6AXuCTOSBhYYPgkqv07FmM5mq8BHyOsXJl7MjV7Zs0Jffonj_Wz9x_4X0KnVJxwq26ALm5HILaYzao-9xB50ADtxtl1kc1zJhcKODp3saif0RzXNO8mxSV-HvixeLIGT-kEHY9MVulaAFMZ6nbN7haU1T3wt_t-ZGz-2fsum9Rz-aQcjABJFPrOFVfEmTzobvsqRj3zvvInyEXrBlxo6BnJFVk303c0qAGooZZ9O63G97Rf3MopI_9V3dIVb7zksV3q8bJ3AE2uhP70_NX";
